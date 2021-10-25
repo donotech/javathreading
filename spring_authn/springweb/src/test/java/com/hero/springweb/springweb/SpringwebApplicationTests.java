1478823f60f1651f3c3ffa508d5d4235ba288706
donotech <devraj@dono.consulting>
2021-10-26 02:54:06 +0530

    added spring cookie authn

---
+++ spring_authn/springweb/src/test/java/com/hero/springweb/springweb/SpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hero.springweb.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
