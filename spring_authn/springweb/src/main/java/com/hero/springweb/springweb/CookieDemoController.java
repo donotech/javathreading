70907891ff523d3eeb6f4ffa4c694c98e392a2ab
donotech <devraj@dono.consulting>
2021-10-27 04:21:17 +0530

    Added raw JWT

--- spring_authn/springweb/src/main/java/com/hero/springweb/springweb/CookieDemoController.java
+++ spring_authn/springweb/src/main/java/com/hero/springweb/springweb/CookieDemoController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("unused")
 @RestController()
 @RequestMapping("cookiedemo")
 public class CookieDemoController {
