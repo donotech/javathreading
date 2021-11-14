c9b4e415d758af665dc82366a27635e7f19f031a
devrajsanyal <devraj@bdec.in>
2021-11-14 22:06:11 +0530

    actuator demo fixed

---
+++ spring_authn/springweb/actuator_demo/src/main/java/com/bdec/hero/spring_web/actuator_demo/ActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bdec.hero.spring_web.actuator_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDemoApplication.class, args);
+	}
+
+}
---
+++ spring_authn/springweb/actuator_demo/src/test/java/com/bdec/hero/spring_web/actuator_demo/ActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdec.hero.spring_web.actuator_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
